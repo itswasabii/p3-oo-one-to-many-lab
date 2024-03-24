950fdd69bf00dc6f60577c802718605beb6cd31d
Gideon Ogot Junior <juniorgideon99@gmail.com>
2024-03-24 23:33:15 +0300

    lab complete

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,32 @@
 class Pet:
+    all = []
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+
+    def __init__(self, name, pet_type, owner=None):
+        if not isinstance(name, str):
+            raise Exception("Name must be a string")
+        self.name = name
+        if pet_type.lower() not in self.PET_TYPES:
+            raise Exception("Invalid pet type")
+        self.pet_type = pet_type.lower()
+        self.owner = owner 
+        self.__class__.all.append(self)
     pass
 
 class Owner:
+    def __init__(self, name):
+        if not isinstance(name, str):
+            raise Exception("Name entered must be a string")
+        self.name = name
+
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+    
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception("Invalid pet type")
+        pet.owner = self
+
+    def get_sorted_pets(self):
+        return sorted(self.pets(), key=lambda pet: pet.name)
     pass
